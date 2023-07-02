b80e2ef74bb6949b8e2e7498a584002fbe917fc1
System Administrator <root@BoosterAlex.local>
2023-07-02 23:18:38 +0300

    create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Github = () => {
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [users, setUsers] = useState([]);
+    const [tempSearch, setTempSearch] = useState('it-kamasutra');
+    const [searchTerm, setSearchTerm] = useState('it-kamasutra');
+    const [userDetails, setUserDetails] = useState([]);
 
-export default App;
+    const fetchData = (term) => {
+        axios
+            .get(`https://api.github.com/search/users?q=${term}`)
+            .then((res) => setUsers(res.data.items));
+    };
+
+    useEffect(() => {
+        if (selectedUser) {
+            document.title = selectedUser;
+        }
+    }, [selectedUser]);
+
+    useEffect(() => {
+        axios
+            .get(`https://api.github.com/search/users?q=${tempSearch}`)
+            .then((res) => setUsers(res.data.items));
+    }, [searchTerm]);
+
+    useEffect(() => {
+        if (!!selectedUser) {
+            axios
+                .get(`https://api.github.com/users/${selectedUser}`)
+                .then((res) => setUserDetails(res.data));
+        }
+    }, [selectedUser]);
+
+    return (
+        <div className='App'>
+            <div>
+                <div>
+                    <input
+                        placeholder='search'
+                        value={tempSearch}
+                        onChange={(e) => {
+                            setTempSearch(e.currentTarget.value);
+                        }}
+                    />{' '}
+                    <button onClick={() => setSearchTerm(tempSearch)}>
+                        Find
+                    </button>
+                </div>
+                <ul>
+                    {users.map((user, i) => (
+                        <li
+                            className={
+                                selectedUser === user.login ? 'selected' : ''
+                            }
+                            key={i}
+                            onClick={() => {
+                                setSelectedUser(user.login);
+                            }}
+                        >
+                            {user.login}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <h2>{selectedUser}</h2>
+                {userDetails && (
+                    <div>
+                        <img src={userDetails.avatar_url} alt='' />
+                        <br />
+                        Login: {userDetails.login}
+                        <br />
+                        Followers: {userDetails.followers}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Github;
